b0b705385c3ca7d163cc7a0d1ed633d853abfab8
marticuadrado <113645533+marticuadrado@users.noreply.github.com>
2022-09-19 18:40:33 -0300

    Terminando el codigo

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 const txtOp1 = document.getElementById("op1")
-const txtOp2 = document.getElementById("op2")
 const txtOperador = document.getElementById("operador")
+const txtOp2 = document.getElementById("op2")
 const btnCalcular = document.getElementById("calcular")
 const pResultado = document.getElementById("resultado")
 
 btnCalcular.addEventListener('click', calcular)
 function calcular(){
-    console.log("apretaste el botón")
+    console.log(txtOperador.value)
+    const operacion = txtOperador.value
+    const op1 = parseFloat(txtOp1.value)
+    const op2 = parseFloat(txtOp2.value)
+
+    if(operacion == "+" || operacion == "-" || operacion == "/" || operacion == "*" && isNaN(op1) == false && isNaN(op2) == false){
+        let resultado;
+        switch (operacion){
+            case "+":
+                resultado = op1 + op2
+                break;
+            case "-":
+                 resultado = op1 - op2
+                break;
+            case "/":
+                resultado = op1 / op2
+                break;
+            case "*":
+                resultado = op1 * op2
+                break;
+        }
+        pResultado.style = "color:black"
+        pResultado.innerText = "= "+ resultado
+    } else {
+        pResultado.style = "color:red"
+        pResultado.innerText = "Cálculo imposible"
+    }
 }
