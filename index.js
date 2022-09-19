78a4e2193fdc23faec2345b2e3b1249dba1dfaff
marticuadrado <113645533+marticuadrado@users.noreply.github.com>
2022-09-18 23:06:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ const btnCalcular = document.getElementById("calcular")
 const pResultado = document.getElementById("resultado")
 
 btnCalcular.addEventListener('click', calcular)
-
+function calcular(){
+    console.log("apretaste el botón")
+}
