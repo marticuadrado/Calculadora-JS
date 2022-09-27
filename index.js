4806c83ebfa42acc0d59b4c995e0c9e3fd02b50f
marticuadrado <113645533+marticuadrado@users.noreply.github.com>
2022-09-27 18:25:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ function calcular(){
     const op1 = parseFloat(txtOp1.value)
     const op2 = parseFloat(txtOp2.value)
 
-    if((operacion == "+" || operacion == "-" || operacion == "/" || operacion == "*") && (!isNaN(op1) && !isNaN(op2))){
+    if((operacion == "+" || operacion == "-" || operacion == "/" || operacion == "*") 
+    && (!isNaN(op1) && !isNaN(op2))){
         let resultado;
         switch (operacion){
             case "+":
